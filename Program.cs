5adf450f0f7f2b905653fedb3267645380613a43
sohail <mohammed.sohail@kalpitatechnologies.com>
2022-08-04 15:52:18 +0530

    mli

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Demo
+{
+    class one
+    {
+        public void test1()
+        {
+            Console.WriteLine("this is class one");
+        }
+    }
+    class two : one
+    {
+        public void test2()
+        {
+            Console.WriteLine("this is class two");
+        }
+    }
+    class three : two
+    {
+        public void test3()
+        {
+            Console.WriteLine("this is class three");
+        }
+    }
+    class Inheritance
+    {
+        static void Main(string[] args)
+        {
+            three t3 = new three();
+            t3.test1();
+            t3.test2();
+            t3.test3();
+        }
+    }
+}
